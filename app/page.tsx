111106f458cadd95399fc20fec881d249897e307
Helio Saraiva <buzato@hotmail.com>
2024-08-06 16:04:43 -0300

    feat: add header components and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
+
 const Home = () => {
-  return <h1 className="text-red-500 bg-red-400 mb-5">Home page</h1>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Helio!</h2>
+        <p>Segunda-Feira, 05 de agosto.</p>
+        <div className="flex items-center gap-2 mt-6">
+        <Input placeholder="Faça sua Busca..."></Input>
+        <Button >
+          <SearchIcon/>
+        </Button>
+        </div>
+        <div className="relative h-[150px] w-full mt-6">
+      <Image
+        src="/banner-01.png"
+        fill
+        className="object-cover rounded-xl"
+        alt="Agende nos melhores com FWS Barber"
+      />
+    </div>
+      </div>      
+    </div>
+  )
 };
 
 export default Home;
