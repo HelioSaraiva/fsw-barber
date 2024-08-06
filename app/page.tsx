dfa7030c4883711af05a70a7bfa63a193728e4dd
Helio Saraiva <buzato@hotmail.com>
2024-08-06 16:56:56 -0300

    feat: add booking

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,61 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return (
     <div>
+      {/* Header */}
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Helio!</h2>
         <p>Segunda-Feira, 05 de agosto.</p>
+        {/* Busca */}
         <div className="flex items-center gap-2 mt-6">
-        <Input placeholder="Faça sua Busca..."></Input>
-        <Button >
-          <SearchIcon/>
-        </Button>
+          <Input placeholder="Faça sua Busca..."></Input>
+          <Button >
+            <SearchIcon />
+          </Button>
         </div>
+        {/* Banner */}
         <div className="relative h-[150px] w-full mt-6">
-      <Image
-        src="/banner-01.png"
-        fill
-        className="object-cover rounded-xl"
-        alt="Agende nos melhores com FWS Barber"
-      />
-    </div>
-      </div>      
+          <Image
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="Agende nos melhores com FWS Barber"
+          />
+        </div>
+
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">
+                Confirmado
+              </Badge>
+              <h3 className="text-center font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className=" h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>              
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 };
